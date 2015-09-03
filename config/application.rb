c2f9ce01125eafc1f13fe9d17fb9965e6063a087
afthab <afthab01@gmail.com>
2015-09-03 17:49:06 +0530

    Heroku_precombilation failed

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
-module Manhood
+config.assets.initialize_on_precompile = false
+module Sample
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
