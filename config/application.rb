2169836aa0d77e05173404e0ce689e1953d4d308
afthab <afthab01@gmail.com>
2015-09-03 18:51:59 +0530

    csfix

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-config.assets.initialize_on_precompile = false
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -9,6 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module Manhood
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
